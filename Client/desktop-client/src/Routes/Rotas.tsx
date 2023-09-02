7293413fe43953dacab5d26de886789d167e1262
Marcelo Augusto <marceloaugustooliveirasoares@gmail.com>
2023-09-01 23:35:33 -0300

    Commit

---
+++ Client/desktop-client/src/Routes/Rotas.tsx
@@ -0,0 +1,7 @@
+import { BrowserRouter, Routes, Route} from 'react-router-dom'
+
+export default function Rotas(){
+    return(<>
+    </>
+    )
+}
--- Database/dbteste.sql
+++
@@ -1,7 +0,0 @@
-create table if not EXISTS Employee(
-id INT AUTOINCREMENT PRIMARY KEY,
-firtName TEXT NOT NULL,
-lastName Text Not null,
-email text not null,
-passwordHash text not null,
-passwordSalt text not null)
